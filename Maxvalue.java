a8904efb1d2c53df18f7015954b80ea5c4bf68fc
jellybeannugget <yusefa2344@gmail.com>
2026-01-29 12:03:43 -0500

    Update Maxvalue.java

--- Maxvalue.java
+++ Maxvalue.java
@@ -7,7 +7,6 @@ public static void main(String[] args) {
     for (int i = 0; i < nums.length; i++){
         if (nums[i] > max){
             max = nums[i];
-            System.out.println(max);
 
         }
     }
